46aa1c760f42f8128d0d10fc35700a765aadb032
jhLee <77824683+nryn6563org@users.noreply.github.com>
2026-02-27 14:25:34 +0900

    chore: remove the entire FAPRO_nuxt application codebase including components, pages, styles, and configuration.

--- FAPRO_nuxt/plugins/echarts.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import * as echarts from 'echarts'
-import ECharts from "vue-echarts";
-
-// Register the component globally
-Vue.component('VChart', ECharts)
--- FAPRO_nuxt/plugins/swiper.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-
-// Import swiper styles
-import 'swiper/swiper-bundle.css'
-
-Vue.use(VueAwesomeSwiper)
