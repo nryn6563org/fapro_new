4ec3f5d6009aef8d51fb88f99b743e31b929b059
jhLee <77824683+nryn6563org@users.noreply.github.com>
2026-03-10 15:55:00 +0900

    로고명 소문자 변경

--- AI_FAPRO/components/layout/AppSidebar.vue
+++ AI_FAPRO/components/layout/AppSidebar.vue
@@ -7,7 +7,7 @@
           <zap-icon size="20" class="text-white" />
         </div>
         <transition name="fade">
-          <span v-if="!isCollapsed" class="app-sidebar__logo-text">AI FA PRO</span>
+          <span v-if="!isCollapsed" class="app-sidebar__logo-text">AI FA Pro</span>
         </transition>
       </div>
     </nuxt-link>
